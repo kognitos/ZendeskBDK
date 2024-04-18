a4fd6e4df10a9e868a7afc295117f09552cd08b1
Emiliano Lesende <emiliano.lesende@gmail.com>
2024-04-18 16:02:19 -0300

    chore: initial commit

---
+++ src/openweather/book.py
@@ -0,0 +1,111 @@
+"""
+A short description of the project.
+"""
+import logging
+from typing import Optional
+from urllib.parse import quote
+
+import requests
+from kognitos.bdk import autoconvert, procedure, book
+from kognitos.bdk.concept import NounPhrase
+
+OPENWEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+DEFAULT_TIMEOUT = 30
+
+# Configure logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@book(icon="data/icon.svg")
+class OpenWeatherBook:
+    """
+    A book for A short description of the project.
+
+    Author:
+      Your Name
+    """
+
+    def __init__(self):
+        """
+        Initializes an instance of the class.
+
+        :param self: The instance of the class.
+        """
+        self._base_url = OPENWEATHER_BASE_URL
+        self._api_key = None
+        self._timeout = float(DEFAULT_TIMEOUT)
+
+    @property
+    def timeout(self) -> float:
+        """
+        Get the value of the timeout.
+
+        Parameters:
+            None
+
+        Returns:
+            The value of the timeout.
+
+        """
+        return self._timeout
+
+    @timeout.setter
+    def timeout(self, timeout: float):
+        """Sets the timeout value in milliseconds.
+
+        Args:
+            timeout (int): The timeout value to set. Must be a positive integer.
+
+        Raises:
+            ValueError: If the timeout value is less than or equal to 0.
+
+        """
+        if timeout <= 0:
+            raise ValueError("timeout must be positive")
+        self._timeout = timeout
+
+    def connect(self, api_key: str):
+        """
+        Connects to an API using the provided API key.
+
+        :param api_key: The API key to use for authentication.
+        :type api_key: str
+        """
+        test_url = f"{self._base_url}?appid={api_key}&q=London"
+        response = requests.get(test_url, timeout=self._timeout)
+        if response.status_code == 401:
+            response_data = response.json()
+            if "Invalid API key" in response_data.get("message", ""):
+                raise ValueError("Invalid API key")
+
+        self._api_key = api_key
+
+    @autoconvert
+    @procedure("to get the current temperature at a city")
+    def current_temperature(self, city: NounPhrase) -> Optional[float]:
+        """Fetch the current temperature for a specified city.
+
+        Args:
+            city (str): The name of the city.
+
+        Returns:
+            Optional[float]: The current temperature in Celsius, or None if an error occurs.
+        """
+        complete_url = f"{self._base_url}?appid={self._api_key}&q={quote(city.to_string())}&units=metric"
+        try:
+            response = requests.get(complete_url, timeout=self._timeout)
+            weather_data = response.json()
+            if weather_data["cod"] == 200:
+                temperature = weather_data["main"]["temp"]
+                return temperature
+
+            logger.error(
+                "error fetching data for %s, response Code: %s", city.to_string(), weather_data['cod']
+            )
+            return None
+        except requests.Timeout:
+            logger.error("request timed out")
+            return None
+        except requests.RequestException as e:
+            logger.error("error occurred: %s", e)
+            return None
---
+++ tests/test_book.py
@@ -0,0 +1,64 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring
+import pytest
+import requests_mock
+
+from openweather import OPENWEATHER_BASE_URL, OpenWeatherBook
+
+API_KEY = "test_api_key"
+
+
+@pytest.fixture
+def openweather_book():
+    book = OpenWeatherBook()
+    return book
+
+
+def test_api_key_valid(openweather_book):
+    with requests_mock.Mocker() as m:
+        m.get(
+            f"{OPENWEATHER_BASE_URL}?appid={API_KEY}&q=London",
+            status_code=200,
+        )
+        openweather_book.connect(API_KEY)
+
+
+def test_api_key_invalid(openweather_book):
+    with requests_mock.Mocker() as m:
+        m.get(
+            f"{OPENWEATHER_BASE_URL}?appid={API_KEY}&q=London",
+            status_code=401,
+            json={"message": "Invalid API key"},
+        )
+        with pytest.raises(ValueError):
+            openweather_book.connect(API_KEY)
+
+
+def test_current_temperature(openweather_book):
+    with requests_mock.Mocker() as m:
+        m.get(
+            f"{OPENWEATHER_BASE_URL}?appid={API_KEY}&q=London",
+            status_code=200,
+        )
+        m.get(
+            f"{OPENWEATHER_BASE_URL}?q=New%20York&appid={API_KEY}&units=metric",
+            json={"cod": 200, "main": {"temp": 20.0}},
+        )
+        openweather_book.connect(API_KEY)
+        temperature = openweather_book.current_temperature("New York")
+        assert temperature == 20.0, "The temperature should be 20.0°C"
+
+
+def test_get_current_temperature_error_response(openweather_book):
+    with requests_mock.Mocker() as m:
+        m.get(
+            f"{OPENWEATHER_BASE_URL}?appid={API_KEY}&q=London",
+            status_code=200,
+        )
+        m.get(
+            f"{OPENWEATHER_BASE_URL}?q=NonExistentCity&appid={API_KEY}&units=metric",
+            json={"cod": 404, "message": "city not found"},
+            status_code=404,
+        )
+        openweather_book.connect(API_KEY)
+        temperature = openweather_book.current_temperature("NonExistentCity")
+        assert temperature is None
